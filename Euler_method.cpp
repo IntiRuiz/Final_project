88c2514938b1ba37366287bd66e117f85cabe85d
IntiRuiz <ilruizc@unal.edu.co>
2016-11-20 20:35:24 -0500

    commit con modificaciones al metodo de Euler y la tabla de datos para la grafica x-vx

--- Euler_method.cpp
+++ Euler_method.cpp
@@ -3,15 +3,9 @@
 #include <cmath>
 
 //declaracion de constantes
-const double miu = 0.012277471;//relacion entre las masas
+const double M = 0.012277471;//relacion entre las masas
 const double T = 17.0652165601579625588917206249;//periodo
 
-/*el sistema de ecuaciones a resolver es:
-f1(p,u,x,y) = dy/dt = p
-g1(p,u,x,y) = dx/dt = u
-g2(p,u,x,y) = du/dt = x-2p-(((1-miu)*(x+miu))/(pow (sqrt(pow((x+miu),2)+pow(y,2)),3)))-((miu*(x-1+miu))/(pow (sqrt(pow((x-1+miu),2)+pow(y,2)),3)))
-f2(p,u,x,y) = dp/dt = y-2u-(((1-miu)*y)/(pow (sqrt(pow((x+miu),2)+pow(y,2)),3)))-((miu*y)/(pow (sqrt(pow((x-1+miu),2)+pow(y,2)),3)))
-*/
 
 //declaracion de funciones
 double f1 (double p, double u, double x, double y);
@@ -29,7 +23,7 @@ int main (void)
   p0 = -2.0015851063790825224053786224;
   h = T/24000;
   
-  for (int ii = 1;ii<=500;ii++){
+  for (int ii = 1;ii<=24000;ii++){
     euler (h,p0,u0,y0,x0);
     std::cout<<x0<<"\t"<<u0<<std::endl;}
   
@@ -43,15 +37,33 @@ double g1 (double p, double u, double x, double y)
 {return u;}
 
 double g2 (double p, double u, double x, double y)
-{return  x-(2*p)-(((1-miu)*(x+miu))/(pow (sqrt(pow((x+miu),2)+pow(y,2)),3)))-((miu*(x-1+miu))/(pow (sqrt(pow((x-1+miu),2)+pow(y,2)),3)));}
+{
+ long double a, b, c, r, s;
+  a=1-M;
+  b=x+M;
+  c=x-1+M;
+  r=std::sqrt(b*b + y*y);
+  s=std::sqrt(c*c + y*y);
+  return x + 2.0*p -(a*b/pow(r,3))-(M*c/pow(s,3));
+}
+
 
 double f2 (double p, double u, double x, double y)
-{return y-(2*u)-(((1-miu)*y)/(pow (sqrt(pow((x+miu),2)+pow(y,2)),3)))-((miu*y)/(pow (sqrt(pow((x-1+miu),2)+pow(y,2)),3)));}
+{
+long double a, b, c, r, s;
+  a=1-M;
+  b=x+M;
+  c=x-1+M;
+  r=std::sqrt(b*b + y*y);
+  s=std::sqrt(c*c + y*y);
+  return y-2.0*u-((a*y)/pow(r,3))-(M*y/pow(s,3));
+}
 
 void euler (double h, double &p0, double &u0, double &y0, double &x0)
 {
-  p0 = p0 + (h*f2(p0,u0,x0,y0));
-  u0 = u0 + (h*g2(p0,u0,x0,y0));
   x0 = x0 + (h*g1(p0,u0,x0,y0));
   y0 = y0 + (h*f1(p0,u0,x0,y0));
+  p0 = p0 + (h*f2(p0,u0,x0,y0));
+  u0 = u0 + (h*g2(p0,u0,x0,y0));
+ 
 }
