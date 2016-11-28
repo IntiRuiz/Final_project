ca5209100187dab8778e1f29429b105c182c466c
dcrianor <dcrianor@unal.edu.co>
2016-11-28 13:08:31 -0800

    commit ya esta bien! :D

--- Euler_method.cpp
+++ Euler_method.cpp
@@ -5,7 +5,7 @@
 //declaracion de constantes
 const double M = 0.012277471;//relacion entre las masas
 const double T = 17.0652165601579625588917206249;//periodo
-
+const double h = T/240000;
 
 //declaracion de funciones
 double f1 (double p, double u, double x, double y);
@@ -16,17 +16,17 @@ void euler (double h, double &p0, double &u0, double &y0, double &x0);
 
 int main (void)
 {
-  double x0, u0, y0, p0, h;
+  double x0, u0, y0, p0;
   x0 = 0.994;
   u0 = 0.0;
   y0 = 0.0;
   p0 = -2.0015851063790825224053786224;
-  h = T/24000;
-  std::cout<<x0<<"\t"<<u0<<std::endl;
-  for (int ii = 0;ii<=24000;ii++){
-    euler (h,p0,u0,y0,x0);
-    std::cout<<x0<<"\t"<<u0<<std::endl;}
+ 
   
+  for (int ii = 0;ii<=240000;ii ++){
+   
+    std::cout<<ii*h<<"\t"<<x0<<std::endl;
+    euler (h,p0,u0,y0,x0);}
   return 0;
 }
 
@@ -44,7 +44,7 @@ double g2 (double p, double u, double x, double y)
   c=x-1+M;
   r=std::sqrt(b*b + y*y);
   s=std::sqrt(c*c + y*y);
-  return x + 2.0*p -(a*b/pow(r,3))-(M*c/pow(s,3));
+  return x + 2.0*p -a*b/pow(r,3)-M*c/pow(s,3);
 }
 
 
@@ -56,14 +56,13 @@ long double a, b, c, r, s;
   c=x-1+M;
   r=std::sqrt(b*b + y*y);
   s=std::sqrt(c*c + y*y);
-  return y-2.0*u-((a*y)/pow(r,3))-(M*y/pow(s,3));
+  return y-2.0*u-(a*y)/pow(r,3)-M*y/pow(s,3);
 }
 
 void euler (double h, double &p0, double &u0, double &y0, double &x0)
 {
-  x0 = x0 + (h*g1(p0,u0,x0,y0));
-  y0 = y0 + (h*f1(p0,u0,x0,y0));
-  p0 = p0 + (h*f2(p0,u0,x0,y0));
-  u0 = u0 + (h*g2(p0,u0,x0,y0));
- 
+  x0 += h*g1(p0,u0,x0,y0);
+  y0 += h*f1(p0,u0,x0,y0);
+  p0 += h*f2(p0,u0,x0,y0);
+  u0 += h*g2(p0,u0,x0,y0);  
 }
--- finalrk4.cpp
+++ finalrk4.cpp
@@ -5,7 +5,7 @@
 //declaracion de constantes
 const long double M = 0.012277471;//relacion entre las masas
 const long double T = 17.0652165601579625588917206249;//periodo
-const long double delta= T/6000;
+const long double delta= T/240000;
 
 long double f0(long double t, long double x0, long double x1, long double y0, long double y1);
 long double f1(long double t, long double x0, long double x1, long double y0, long double y1);
@@ -15,16 +15,15 @@ void rk4(long double t, long double h, long double  &x0, long double &x1, long d
 
 int main(void)
 {
-  long double x, vx, y, vy, time;
+  long double x, vx, y, vy,time;
   x=0.994;
   vx=0.0;
   y=0.0;
   vy=-2.0015851063790825224053786224;
-  for(time=0.0; time<=2*T; time+=delta)
+  for( time=0; time<=T; time+=delta)
     {
+      std::cout<<x<<"\t"<<y<<std::endl;
       rk4(time, delta, x, vx, y, vy);
-      std::cout<<x<<"\t"<<vx<<std::endl;
-      
     }
   return 0;
 }
@@ -42,7 +41,7 @@ long double f1(long double t, long double x0, long double x1, long double y0, lo
   c=x0-1+M;
   r=std::sqrt(b*b + y0*y0);
   s=std::sqrt(c*c + y0*y0);
-  return x0 + 2.0*y1 -(a*b/pow(r,3))-(M*c/pow(s,3));
+  return x0 + 2.0*y1 -a*b/pow(r,3)-M*c/pow(s,3);
 }
 
 long double g0(long double t, long double y0, long double y1, long double x0, long double x1)
@@ -58,7 +57,7 @@ long double g1(long double t, long double y0, long double y1, long double x0, lo
   c=x0-1+M;
   r=std::sqrt(b*b + y0*y0);
   s=std::sqrt(c*c + y0*y0);
-  return y0-2.0*x1-((a*y0)/pow(r,3))-(M*y0/pow(s,3));
+  return y0-2.0*x1-a*y0/pow(r,3)-M*y0/pow(s,3);
 }												  
 
 void rk4(long double t, long double h, long double &x0, long double &x1, long double &y0, long double &y1)
@@ -68,15 +67,15 @@ void rk4(long double t, long double h, long double &x0, long double &x1, long do
 
   k10=h*f0(t, x0, x1, y0, y1);
   k11=h*f1(t, x0, x1, y0, y1);
-  k20=h*f0(t+ h/2, x0+ k10/2, x1+ k11/2, y0, y1);
-  k21=h*f1(t+ h/2, x0+ k10/2, x1+ k11/2, y0, y1);
-  k30=h*f0(t+ h/2, x0+ k20/2, x1+ k21/2, y0, y1);
-  k31=h*f1(t+ h/2, x0+ k20/2, x1+ k21/2, y0, y1);
+  k20=h*f0(t+ (h/2), x0+ (k10/2), x1+ (k11/2), y0, y1);
+  k21=h*f1(t+ (h/2), x0+ (k10/2), x1+ (k11/2), y0, y1);
+  k30=h*f0(t+ (h/2), x0+ (k20/2), x1+ (k21/2), y0, y1);
+  k31=h*f1(t+ (h/2), x0+ (k20/2), x1+ (k21/2), y0, y1);
   k40=h*f0(t+h, x0+k30, x1+k31, y0, y1);
   k41=h*f1(t+h, x0+k30, x1+k31, y0, y1);
 
-  x0= x0+ (1.0/6.0)*(k10+(2*k20)+(2*k30)+k40);
-  x1= x1+ (1.0/6.0)*(k11+(2*k21)+(2*k31)+k41);
+  x0+=(1.0/6.0)*(k10+(2*k20)+(2*k30)+k40);
+  x1+=(1.0/6.0)*(k11+(2*k21)+(2*k31)+k41);
 
   p10=h*g0(t, y0, y1, x0, x1);
   p11=h*g1(t, y0, y1, x0, x1);
@@ -87,8 +86,8 @@ void rk4(long double t, long double h, long double &x0, long double &x1, long do
   p40=h*g0(t+h, y0+p30, y1+p31, x0, x1);
   p41=h*g1(t+h, y0+p30, y1+p31, x0, x1);
 
-  y0= y0+ (1.0/6.0)*(p10+(2*p20)+(2*p30)+p40);
-  y1= y1+ (1.0/6.0)*(p11+(2*p21)+(2*p31)+p41);
+  y0+=(1.0/6.0)*(p10+(2*p20)+(2*p30)+p40);
+  y1+=(1.0/6.0)*(p11+(2*p21)+(2*p31)+p41);
 }
 
 
